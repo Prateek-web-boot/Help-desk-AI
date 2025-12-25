ec45f31934e3ac859cbbabe152835c7906410362
PrateekGupta <prateek.gupta1@traiana.com>
2025-12-25 15:21:18 +0530

    - Added JBDC Repository - changed the REST url - updated the advisor to include conversation ID - created seprate for Email notification - updated code to allow one-to-many entries

--- src/main/java/com/substring/helpdesk/config/AiConfig.java
+++ src/main/java/com/substring/helpdesk/config/AiConfig.java
@@ -1,21 +1,33 @@
 package com.substring.helpdesk.config;
 
-import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
-import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.MessageWindowChatMemory;
+import org.springframework.ai.chat.memory.repository.jdbc.JdbcChatMemoryRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AiConfig {
 
+    Logger logger = LoggerFactory.getLogger(AiConfig.class);
+
     @Bean
-    public ChatClient chatClient(ChatClient.Builder builder, ChatMemory chatMemory) {
+    public ChatClient chatClient(ChatClient.Builder builder, JdbcChatMemoryRepository jdbcChatMemoryRepository) {
+
+        var chatMemory = MessageWindowChatMemory.builder()
+                .chatMemoryRepository(jdbcChatMemoryRepository)
+                .maxMessages(15)
+                .build();
+
+        logger.info("Chat Client bean created");
+        logger.info("Chat Memory bean created, {}", chatMemory.getClass().getName());
 
         return builder
-                .defaultAdvisors(new SimpleLoggerAdvisor(), MessageChatMemoryAdvisor.builder(chatMemory).build())
-                .build();
+                .defaultAdvisors(new SimpleLoggerAdvisor(),
+                        MessageChatMemoryAdvisor.builder(chatMemory).build()).build();
     }
 }
