805dc657979d73100af70e36ae9a2bbe63f54246
PrateekGupta <prateek.gupta1@traiana.com>
2025-12-29 23:58:41 +0530

    added exception handler annotation

--- src/main/java/com/substring/helpdesk/exception/GlobalExceptionHandler.java
+++ src/main/java/com/substring/helpdesk/exception/GlobalExceptionHandler.java
@@ -3,12 +3,14 @@ package com.substring.helpdesk.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import java.util.Map;
 
 @Controller
 public class GlobalExceptionHandler {
 
+    @ExceptionHandler(Exception.class)
     public ResponseEntity<Object> handleAllExceptions(Exception ex, WebRequest request) {
         return new ResponseEntity<>(
                 Map.of("message", ex.getMessage(), "status", "error"),
