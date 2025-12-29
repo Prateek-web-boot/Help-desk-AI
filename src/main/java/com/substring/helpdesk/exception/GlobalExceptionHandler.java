98b2adf7db1282dcfcd9ee690fdbbd29a7ffbbce
PrateekGupta <prateek.gupta1@traiana.com>
2025-12-29 23:59:04 +0530

    modified annotation

--- src/main/java/com/substring/helpdesk/exception/GlobalExceptionHandler.java
+++ src/main/java/com/substring/helpdesk/exception/GlobalExceptionHandler.java
@@ -2,12 +2,12 @@ package com.substring.helpdesk.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import java.util.Map;
 
-@Controller
+@ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
